962a87f252f626bb0956c3a9fe7f65c212f4efec
max cohen <max.cohen@gmx.com>
2019-01-14 16:14:53 +0100

    removed stupid reversed

--- DBN.py
+++ DBN.py
@@ -1,22 +1,20 @@
 from RBM import RBM
 
 class DBN():
-    def __init__(self, layers=3, n_h=[10, 16, 32, 784]):
-        self.layers = [RBM(n_h[i], n_h[i+1]) for i in range(len(n_h)-1)]
+    def __init__(self, n_h):
+        self.layers = [RBM(n_h[i+1], n_h[i]) for i in range(len(n_h)-1)]
         
     def train(self, X, epochs=5, lr=1e-3):
-        for layer in reversed(self.layers):
+        for layer in self.layers:
             layer.train(X, epochs=epochs, lr=lr)
             X = layer.forward(X)
             
     def forward(self, X):
-        Y = X
-        for layer in reversed(self.layers):
-            Y = layer.forward(Y)
-        return Y
+        for layer in self.layers:
+            X = layer.forward(X)
+        return X
     
     def backward(self, H):
-        A = H
-        for layer in self.layers:
-            A = layer.backward(A)
-        return A
+        for layer in reversed(self.layers):
+            H = layer.backward(H)
+        return H
