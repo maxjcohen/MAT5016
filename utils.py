bed10025fc0b78fde0eac212a5bbeca0af9cec87
max cohen <max.cohen@gmx.com>
2019-01-21 17:32:46 +0100

    Added softmax function

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
 def show_img(img):
     plt.imshow(img, cmap='gray')
+    
+def softmax(z):
+    a = np.exp(z)
+    return a / np.sum(a, axis=-1, keepdims=True)
