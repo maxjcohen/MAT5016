5321f9bde661b2376890a37e7e167fef30f7bd9e
max cohen <max.cohen@gmx.com>
2019-01-23 11:09:24 +0100

    Added arg for no pretraining last layer (see DNN)

--- DBN.py
+++ DBN.py
@@ -4,8 +4,14 @@ class DBN():
     def __init__(self, n_h):
         self.layers = [RBM(n_h[i+1], n_h[i]) for i in range(len(n_h)-1)]
         
-    def train(self, X, epochs=5, lr=1e-3):
-        for layer in self.layers:
+        self.L = len(self.layers)
+        
+    def train(self, X, epochs=5, lr=1e-3, except_last=False):
+        for l, layer in enumerate(self.layers):
+            
+            if except_last and l == self.L-1:
+                return
+            
             layer.train(X, epochs=epochs, lr=lr)
             X = layer.forward(X)
             
