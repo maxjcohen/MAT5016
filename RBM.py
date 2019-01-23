c3c94e9bbf4dfa4d4a2dbc7adc4aba8ccb187437
max cohen <max.cohen@gmx.com>
2019-01-23 11:49:08 +0100

    Added batch training

--- RBM.py
+++ RBM.py
@@ -23,35 +23,41 @@ class RBM():
     def backward(self, H):
         return self.activation(np.dot(H, self.W) + self.a)
     
-    def train(self, X, epochs=5, lr=1e-3):
+    def train(self, X, epochs=5, lr=1e-3, batch_size=32):
         m = len(X)
         losses = np.zeros(epochs)
         
-        v_0 = X
-        
-        for epoch in tqdm(range(epochs)):
+        num_batches = (m + batch_size - 1) // batch_size
+        idx_batches = [(i * batch_size, min(m-1, (i + 1) * batch_size)) for i in range(num_batches)]
 
-            # Sample h_0
-            p_h_v_0 = self.forward(v_0)
-            h_0 = p_h_v_0 >= np.random.random(p_h_v_0.shape)
-
-            # Sample v_1
-            p_v_h_0 = self.backward(h_0)
-            v_1 = p_v_h_0 >= np.random.random(p_v_h_0.shape)
-            
-            # Loss
-            losses[epoch] = ((v_0 - v_1) ** 2).mean()
-
-            p_h_v_1 = self.forward(v_1)
-
-            # Compute deltas
-            dw = np.dot(v_0.T, p_h_v_0) - np.dot(v_1.T, p_h_v_1)
-            da = np.sum(v_0 - v_1, axis=0)
-            db = np.sum(p_h_v_0 - p_h_v_1, axis=0)
-
-            # Update weights
-            self.W += lr * dw.T
-            self.a += lr * da.T
-            self.b += lr * db.T
+        for e in tqdm(range(epochs)):
+            for idx_b, idx_e in idx_batches:
+                self.backward_forward(X[idx_b:idx_e, ...], lr=lr)
             
         plt.plot(losses)
+        
+    def backward_forward(self, X, lr=1e-3):
+        v_0 = X
+        
+        # Sample h_0
+        p_h_v_0 = self.forward(v_0)
+        h_0 = p_h_v_0 >= np.random.random(p_h_v_0.shape)
+
+        # Sample v_1
+        p_v_h_0 = self.backward(h_0)
+        v_1 = p_v_h_0 >= np.random.random(p_v_h_0.shape)
+
+        # Loss
+#         losses[epoch] = ((v_0 - v_1) ** 2).mean()
+
+        p_h_v_1 = self.forward(v_1)
+
+        # Compute deltas
+        dw = np.dot(v_0.T, p_h_v_0) - np.dot(v_1.T, p_h_v_1)
+        da = np.sum(v_0 - v_1, axis=0)
+        db = np.sum(p_h_v_0 - p_h_v_1, axis=0)
+
+        # Update weights
+        self.W += lr * dw.T
+        self.a += lr * da.T
+        self.b += lr * db.T
