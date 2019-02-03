06fc86ab18aa4d406401ad18254055cd0059c8fa
max cohen <max.cohen@gmx.com>
2019-02-03 19:32:10 +0100

    Added acc computing on test dataset

--- DNN.py
+++ DNN.py
@@ -50,9 +50,12 @@ class DNN(DBN):
             layer.W += - lr * dWs[l]
             layer.b += - lr * dbs[l]
     
-    def train_supervised(self, X, Y, epochs, batch_size=32, lr=1e-3):
+    def train_supervised(self, X, Y, epochs, batch_size=32, lr=1e-3, validation_data=None):
         m = X.shape[0]
         
+        if validation_data:
+            X_test, Y_test = validation_data
+            
         hist_loss = []
         
         num_batches = (m + batch_size - 1) // batch_size
@@ -61,10 +64,16 @@ class DNN(DBN):
         for e in range(epochs):
             for idx_b, idx_e in idx_batches:
                 self.backpropagation(X[idx_b:idx_e, ...], Y[idx_b:idx_e, ...], lr=lr)
-
-            Y_hat = self.forward(X)
-            loss = self.compute_loss(Y, Y_hat)
-            acc = self.compute_acc(Y, Y_hat)
+            
+            if validation_data:
+                Y_hat = self.forward(X_test)
+                loss = self.compute_loss(Y_test, Y_hat)
+                acc = self.compute_acc(Y_test, Y_hat)
+            else:
+                Y_hat = self.forward(X)
+                loss = self.compute_loss(Y, Y_hat)
+                acc = self.compute_acc(Y, Y_hat)
+                
             print(f'Loss: {np.round(loss, 3)}\tAcc: {np.round(acc, 3)}')
             hist_loss.append(loss)
         plt.plot(hist_loss)
